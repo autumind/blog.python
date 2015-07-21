f362a7d43de1b4d3e854e2cd4528739b30cea348
Coding Ape <autumind@gmail.com>
2015-07-21 08:42:01 +0800

    Update model.py

--- www/model.py
+++ www/model.py
@@ -1,23 +1,24 @@
 # -*- coding:utf-8 -*-
 import web, datetime
 
-db = web.database(dbn='mysql',user='shen', pw='shen', db='blog', charset='utf8')
-#db = web.database(dbn='sqlite', db='.\\DATA\\blog.db')
+#db = web.database(dbn='mysql',user='shen', pw='shen', db='blog', charset='utf8')
+db = web.database(dbn='sqlite', db='.\\DATA\\blog.db')
 def get_posts():
-    return db.select('entries', order='id DESC')
+    return db.query("SELECT id, title, content, datetime(posted_on) as posted_on FROM blogs order by id")
+    #return db.select('entries', order='id DESC')
 
 def get_post(id):
     try:
-        return db.select('entries', where='id=$id', vars=locals())[0]
+        return db.select('blogs', where='id=$id', vars=locals())[0]
     except IndexError:
         return None
 
 def new_post(title, text):
-    db.insert('entries', title=title, content=text, posted_on=datetime.datetime.utcnow())
+    db.insert('blogs', title=title, content=text, posted_on=datetime.datetime.now())
 
 def del_post(id):
-    db.delete('entries', where="id=$id", vars=locals())
+    db.delete('blogs', where="id=$id", vars=locals())
 
 def update_post(id, title, text):
-    db.update('entries', where="id=$id", vars=locals(),
+    db.update('blogs', where="id=$id", vars=locals(),
         title=title, content=text)
